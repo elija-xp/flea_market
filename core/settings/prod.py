c0daa1fbbaf8499bac7399257e4c66f779fe2253
elija-xp <eyelja@gmail.com>
2026-05-31 18:30:15 +0300

    trying to fix registratioin error

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -26,3 +26,18 @@ DATABASES = {
 }
 
 STATIC_ROOT = "staticfiles/"
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'ERROR',
+    },
+}
